a6b75fb2f478cf082a35c5a59fd680b4fcff1462
MArouri <mohammad.arouri@gmail.com>
2013-12-29 18:56:20 +0200

    Add JavaDoc Plugin

---
+++ JenkinsTest/src/test/java/com/mixberry/test/TestMainApp.java
@@ -0,0 +1,13 @@
+package com.mixberry.test;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class TestMainApp extends TestCase {
+
+	@Test
+	public void test(){
+		assertEquals(true, true);
+	}
+}

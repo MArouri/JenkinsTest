e88b8ff54a18f6b4b8270a3aa75b3d82530c6995
MArouri <mohammad.arouri@gmail.com>
2013-12-29 18:16:49 +0200

    Junit Simple.

---
+++ JenkinsTest/src/main/java/com/mixberry/test/TestMainApp.java
@@ -0,0 +1,14 @@
+package com.mixberry.test;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+
+public class TestMainApp extends TestCase {
+
+	@Test
+	public void simpleTest(){
+		assertEquals("1", "1");
+	}
+}
